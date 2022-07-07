7a7e4505395bc22bc849709f398dc77137f44361
kevinstauffer <108687801+kevinstauffer@users.noreply.github.com>
2022-07-07 21:33:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function pressButton(symbol) {
-    document.getElementById("kevin").innerHTML = symbol;
-    let computer = computerSpielt();
-    document.getElementById("computer").innerHTML = computer;
-    //
-}
+const Auswahl = ["Schere ✂️", "Stein 🪨", "Papier 📑"]
 
-const Auswahl = ["schere", "stein", "papier"]
+function pressButton(kevinSymbol) {
+    document.getElementById("kevin").innerHTML = kevinSymbol;
+    let computerSymbol = Auswahl[Math.floor(Math.random() * 3)]
+    document.getElementById("computer").innerHTML = computerSymbol;
+    if (kevinSymbol === computerSymbol) {
+        document.getElementById("gewinner").innerHTML = "Unentschieden";
 
-function computerSpielt() {
-    let computersymbol = Auswahl[Math.floor(Math.random() * 2)]
-
-    return computersymbol
+    } else if (kevinSymbol === 'Stein 🪨' && computerSymbol === 'Schere ✂️') {
+        document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+    } else if (kevinSymbol === 'Schere ✂️' && computerSymbol === 'Papier 📑') {
+        document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+    } else if (kevinSymbol === 'Papier 📑' && computerSymbol === 'Stein 🪨') {
+        document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+    } else if (computerSymbol === 'Stein 🪨' && kevinSymbol === 'Schere ✂️') {
+        document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+    } else if (computerSymbol === 'Schere ✂️' && kevinSymbol === 'Papier 📑') {
+        document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+    } else if (computerSymbol === 'Papier 📑' && kevinSymbol === 'Stein 🪨') {
+        document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+    }
 }
