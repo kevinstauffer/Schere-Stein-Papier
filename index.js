a4eb5998c21b172bcfbf027efc93280c8fd099eb
kevinstauffer <108687801+kevinstauffer@users.noreply.github.com>
2022-07-13 18:38:33 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Auswahl = ["Schere ✂️", "Stein 🪨", "Papier 📑"]
+let punkteKevin = 0
+let punkteComputer = 0
+let punkteUnentschieden = 0
+let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
 
 function pressButton(kevinSymbol) {
     document.getElementById("kevin").innerHTML = kevinSymbol;
@@ -6,18 +10,56 @@ function pressButton(kevinSymbol) {
     document.getElementById("computer").innerHTML = computerSymbol;
     if (kevinSymbol === computerSymbol) {
         document.getElementById("gewinner").innerHTML = "Unentschieden";
+        punkteUnentschieden += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+        console.log(punkteStandOverAll)
+        console.log(punkteUnentschieden)
+
+
 
     } else if (kevinSymbol === 'Stein 🪨' && computerSymbol === 'Schere ✂️') {
         document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+        punkteKevin += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     } else if (kevinSymbol === 'Schere ✂️' && computerSymbol === 'Papier 📑') {
         document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+        punkteKevin += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     } else if (kevinSymbol === 'Papier 📑' && computerSymbol === 'Stein 🪨') {
         document.getElementById("gewinner").innerHTML = "🏆 Kevin gewinnt";
+        punkteKevin += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     } else if (computerSymbol === 'Stein 🪨' && kevinSymbol === 'Schere ✂️') {
         document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+        punkteComputer += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     } else if (computerSymbol === 'Schere ✂️' && kevinSymbol === 'Papier 📑') {
         document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+        punkteComputer += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     } else if (computerSymbol === 'Papier 📑' && kevinSymbol === 'Stein 🪨') {
         document.getElementById("gewinner").innerHTML = "😞 Computer gewinnt";
+        punkteComputer += 1
+        let punkteStandOverAll = (punkteKevin + ' / ' + punkteComputer + ' / ' + punkteUnentschieden)
+
+        document.getElementById('PunkteStand').innerHTML = punkteStandOverAll
+
     }
 }
